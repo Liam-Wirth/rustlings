5f4eeb161ec63cf0764529091fed83ce52ee1ed0
liam <liam.wirth1@outlook.com>
2022-06-06 10:43:21 -0700

    jeff

---
+++ target/debug/deps/argh_shared-e2530acab387cb1e.d
@@ -0,0 +1,5 @@
+c:\Users\zambi\rustlings\target\debug\deps\argh_shared-e2530acab387cb1e.rmeta: C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\argh_shared-0.1.5\src\lib.rs
+
+c:\Users\zambi\rustlings\target\debug\deps\argh_shared-e2530acab387cb1e.d: C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\argh_shared-0.1.5\src\lib.rs
+
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\argh_shared-0.1.5\src\lib.rs:
