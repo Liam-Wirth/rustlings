c88d99b252a08bd48d102650c4454f8c1b53b956
liam <liam.wirth1@outlook.com>
2022-06-06 10:45:37 -0700

    fug

---
+++ target/debug/deps/walkdir-35bf5c074540caa3.d
@@ -0,0 +1,8 @@
+c:\Users\zambi\rustlings\target\debug\deps\walkdir-35bf5c074540caa3.rmeta: C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\lib.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\dent.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\error.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\util.rs
+
+c:\Users\zambi\rustlings\target\debug\deps\walkdir-35bf5c074540caa3.d: C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\lib.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\dent.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\error.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\util.rs
+
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\lib.rs:
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\dent.rs:
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\error.rs:
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\walkdir-2.3.2\src\util.rs:
---
+++ target/debug/deps/winapi_util-532149c81e355d63.d
@@ -0,0 +1,8 @@
+c:\Users\zambi\rustlings\target\debug\deps\winapi_util-532149c81e355d63.rmeta: C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\lib.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\console.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\file.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\win.rs
+
+c:\Users\zambi\rustlings\target\debug\deps\winapi_util-532149c81e355d63.d: C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\lib.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\console.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\file.rs C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\win.rs
+
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\lib.rs:
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\console.rs:
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\file.rs:
+C:\Users\zambi\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-util-0.1.5\src\win.rs:
