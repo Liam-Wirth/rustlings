3c50dc6350346d9e1a1547ac4c397e9b77339906
Liam-Wirth <liam.wirth1@outlook.com>
2022-07-26 10:06:32 -0700

    i lov e neovim

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,13 +1,14 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
+//ohhhh I'm supposed to put something here... I get it now
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    //I'm gonna be honest here, I'm following someone elses solution cause I have no experience with c lang,is this person opting for classic strings cause they are more memmory efficient? or just laziness?
+    name: &'static str,
+    hex: &'static str,
 }
-
-struct ColorTupleStruct(/* TODO: Something goes here */);
+//lmaooo I accidentally solved this one by just implementing a tuple, I'll comment out that code and do it right :/
+struct ColorTupleStruct(&'static str, &'static str);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -19,8 +20,10 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
-
+        let green = ColorClassicStruct {
+            name: "green",
+            hex: "#00FF00",
+        };
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
     }
@@ -28,16 +31,16 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green = (String::from("green"), "#00FF00");
-
+        //   let green = (String::from("green"), "#00FF00");
+        let green = ColorTupleStruct("green", "#00FF00");
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
     }
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        // TODO: Instantiate a unit struct! ??
+        let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
